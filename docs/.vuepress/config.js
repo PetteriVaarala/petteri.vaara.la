7f0cb2778889468c8feb963bcefb0e6369f8b709
Petteri Vaarala <petteri.vaarala@gmail.com>
2022-02-11 23:56:08 +0200

    Add Vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  title: 'Just some random notes',
+  description: '',
+  theme: 'casper',
+  markdown: {
+    anchor: {
+      permalink: false,
+      permalinkBefore: false
+    }
+  },
+  themeConfig: {
+    navbar: true,
+    nav: [
+      { text: 'home', link: '/' },
+      { text: 'about', link: '/about/' },
+    ],
+    footer: [
+      {
+        text: "Latest Posts",
+        link: "/posts"
+      },
+      {
+        text: "Tags",
+        link: "/tags"
+      },
+      {
+        text: "Categories",
+        link: "/category"
+      }
+    ],
+    social: {
+      github: "https://github.com/PetteriVaarala",
+      linkedin: "https://www.linkedin.com/in/petterivaarala/"
+    },
+    defaultAuthor: {
+      link: "https://petteri.vaara.la",
+      name: "Petteri Vaarala"
+    },
+    search: true
+  }
+}
---
+++ docs/2020/firefox-pdf-default-zoom.md
@@ -0,0 +1,15 @@
+---
+title: Firefox PDF Reader Default Zoom
+publish: 2020-10-22
+type: post
+tags:
+  - firefox
+  - foobar
+---
+
+about:config
+
+```
+pdfjs.defaultZoomValue: page-fit
+```
+<!-- more -->
