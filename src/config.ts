37b79b648a2f22f3377d899d7f5e55cca127f3f4
Petteri Vaarala <petteri.vaarala@gmail.com>
2022-10-27 17:17:37 +0300

    More cleanups

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 import packageJson from '../package.json';
 
 export const SITE_TITLE = packageJson.name;
