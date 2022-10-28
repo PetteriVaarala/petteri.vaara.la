b4e33b0edc511045f9f4ce9e59ba24b6ccdc394c
Petteri Vaarala <petteri.vaarala@gmail.com>
2022-10-28 20:35:51 +0300

    Index page with fullscreen blurred background image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: "class",
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: (theme) => ({
+				programmer: "url('/unsplash/chris-ried-ieic5Tq8YMk-unsplash-2560.jpg')",
+				mtbr: "url('/unsplash/julian-hochgesang-jyoTLVMv9So-unsplash-2560.jpg')",
+			}),
+		},
+	},
+	variants: {
+		extend: {
+			backgroundImage: ["programmer"],
+		},
 	},
 	plugins: [],
 };
