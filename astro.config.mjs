95a8fa6200e7d08bb99b8273d911396a43f67b63
Petteri Vaarala <petteri.vaarala@gmail.com>
2025-10-05 00:10:57 +0300

    Ditch Deno and use node/pnpm to get Tailwindcss v4 updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://notes.petteri.vaara.la",
   outDir: "public",
   publicDir: "static",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
