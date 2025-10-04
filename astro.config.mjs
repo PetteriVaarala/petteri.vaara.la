68e3946312248da6f7cbb6c44c341e85074b1ddd
Petteri Vaarala <petteri.vaarala@gmail.com>
2025-10-04 23:26:56 +0300

    Deno linting and formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://notes.petteri.vaara.la',
-	outDir: 'public',
-	publicDir: 'static',
-	integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://notes.petteri.vaara.la",
+  outDir: "public",
+  publicDir: "static",
+  integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import packageJson from '../package.json';
+import packageJson from "../package.json";
 
 export const SITE_TITLE = packageJson.name;
 export const SITE_DESCRIPTION = packageJson.description;
